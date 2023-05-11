50989c71a529c79ca38948394eca8b97c60c0920
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-11 18:36:29 +0200

    feat(user): finish user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Patch, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,6 +11,26 @@ export class UsersController {
 
   @Post('signup')
   createUser(@Body() body: CreateUserDto){
-    this.usersService.create(body.email, body.password); 
+    return this.usersService.create(body.email, body.password); 
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string){
+    return this.usersService.findOne(+id);
+  }
+
+  @Get()
+  findAllUser(@Query('email') email: string){
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+    return this.usersService.update( +id, body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
