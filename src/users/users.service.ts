f9b066ce5aa5bb620a464a16eb320e3d2f91c2c1
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-11 18:17:13 +0200

    feat(user): add findOne find update and remove user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Post } from '@nestjs/common';
+import { Injectable, Param, Post } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import  {InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -9,6 +9,32 @@ export class UsersService {
 
   create(email: string, password: string) {
     const user = this.repo.create({ email, password});
-    return this.repo.save(user);
+    return this.repo.insert(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({id});
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: {email} });
+  }
+
+  async update(id: number, attrs: Partial<User> ) { 
+    const user = await this.findOne(id);
+    if(!user) {
+      throw new Error('user not found!')
+    }
+    Object.assign(user, attrs);
+    return this.repo.update( id, user )
+  }
+
+  async remove(id: number){
+    const user = await this.findOne(id);
+    if(!user) {
+      throw new Error('user not found!')
+    }
+
+    return this.repo.remove( user );
   }
 }
