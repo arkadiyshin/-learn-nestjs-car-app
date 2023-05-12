b0ea5bec72d0cfe0dff5f05c2b137ff6b4872a4c
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-12 15:45:10 +0200

    test(auth): authService to be defined

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userService: UsersService;
+  const fakeUserService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) => Promise.resolve( {id: 1, email, password} )
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService
+        }
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
