d5ad585ae82969f4c4c4a893b2a2aabf35219972
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-12 10:58:50 +0200

    feat(auth): create a hash password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,19 +1,25 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService) { }
 
 
   async signup(email: string, password: string) {
     const users = await this.userService.find(email)
-    if(users.length > 0) {
+    if (users.length > 0) {
       throw new BadRequestException('email in use')
     }
 
-    const hash = (password);
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = `${salt}.${hash.toString('hex')}`;
+
   }
 
   signin(email: string, password: string) {
