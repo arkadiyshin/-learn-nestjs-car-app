2baf16d459c9d892f822697fe524584ba03d2957
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-12 13:06:01 +0200

    feat(auth): add signout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -34,10 +34,15 @@ export class UsersController {
   }
 
   @Post('signin')
-  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+  async signIn(@Body() body: CreateUserDto, @Session() session: any) {
     const user = await this.authService.signin(body.email, body.password);
     session.userId = user.id;
     return user;
+  }
+
+  @Post('sighout')
+  signOut(@Session() session: any){
+    session.userId = null;
   }
 
   @Get('/:id')
