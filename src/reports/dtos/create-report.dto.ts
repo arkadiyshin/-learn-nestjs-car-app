0b3b6d4fff67d1b4002fc1ddcedc61f5734711a7
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-14 19:13:45 +0200

    feat(report): add report dto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,30 @@
+import { IsString, IsEmail, IsNumber, IsLongitude, IsLatitude, Max, Min } from "class-validator";
+
+export class CreateReportDto {
+  @IsString()
+  make:string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  milage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+}
