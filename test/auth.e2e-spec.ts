1d4c5fa376179dbf717eb2d1e434e229239dbdf5
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-14 15:12:26 +0200

    test(e2e): route: signup

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handels a singup request', () => {
+    
+    const email = 'unique2@gmail.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: "password"})
+      .expect(201).
+      then( (res) => {
+        const {id, email} = res.body;
+        expect(id).toBeDefined;
+        expect(email).toEqual(email);
+      })
+      
+  });
+
+
+});
