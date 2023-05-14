fadff83446ef3e5a2ca732b3bf76f37d4ba96ec8
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-14 18:57:56 +0200

    feat(report): add properties to report

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -8,4 +8,21 @@ export class Report {
   @Column()
   price: number;
 
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number
+
+  @Column()
+  lat: number;
+
+  @Column()
+  milage: number;
 }
