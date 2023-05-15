cbed97a753556b93ea54158737746e3220feb6d5
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-15 12:33:25 +0200

    feat(user): assign current user with middleware

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,22 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import { UsersService } from "../users.service";
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(
+    private userService: UsersService
+  ) { }
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.session || {};
+
+    if (userId) {
+      const user = this.userService.findOne(userId);
+      // @ts-ignore
+      req.currentUser = user;
+    }
+
+    next();
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
-import { CurrentUserInterceptor } from './Interceptors/current-user.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -13,10 +12,12 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
   providers: [
     UsersService,
     AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
   ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CurrentUserMiddleware)
+      .forRoutes('*');
+  }
+}
