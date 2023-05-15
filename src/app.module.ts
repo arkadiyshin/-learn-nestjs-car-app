932156d9bff9c7b54c4c177fd6dbde7ade2ed058
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-15 14:42:22 +0200

    refactor(app): turn hardcode cookie key to env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,11 +43,15 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(
+    private configService: ConfigService
+  ) { }
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdfghjkl'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
