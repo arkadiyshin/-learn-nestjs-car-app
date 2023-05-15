094827108f08000740b4f0979fadc23bff807148
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-15 12:45:02 +0200

    fix(user): fix type current user definition in middleware

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import { UsersService } from "../users.service";
+import { User } from "../user.entity";
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -12,8 +21,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
 
     if (userId) {
-      const user = this.userService.findOne(userId);
-      // @ts-ignore
+      const user = await this.userService.findOne(userId);
       req.currentUser = user;
     }
 
